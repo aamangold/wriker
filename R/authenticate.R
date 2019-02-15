66e0192114625bb31019323b9cc70d140509a5ea
Alexandra Mangold <alexandra.a.mangold@gmail.com>
2019-02-15 15:56:25 -0500

    Added error message if environment variable authentication is blank. https://cran.r-project.org/web/packages/httr/vignettes/api-packages.html

--- R/authenticate.R
+++ R/authenticate.R
@@ -13,6 +13,15 @@
 authenticate <- function() {
 
     account_id <<- Sys.getenv('WRIKE_ACCOUNT_ID')
+    if (identical(account_id, "")) {
+        stop("Please set env var WRIKE_ACCOUNT_ID",
+             call. = FALSE)
+    }
+    
     v3_key <<- Sys.getenv('WRIKE_V3_KEY')
+    if (identical(v3_key, "")) {
+        stop("Please set env var WRIKE_V3_KEY",
+             call. = FALSE)
+    }
 
 }
